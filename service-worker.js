427c14093fa8726f38496eaaa2501eda2273600e
SergeyDavidovich <writesd@hotmail.com>
2021-07-11 09:41:04 +0000

    Deploying to gh-pages from  @ 593fba7b9ca32b8baf225900d4be4c1da4549751 🚀

--- service-worker.js
+++ service-worker.js
@@ -18,10 +18,10 @@ async function onInstall(event) {
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash }));
+        .map(asset => new Request(asset.url));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
-
+        //.map(asset => new Request(asset.url, { integrity: asset.hash }));
 async function onActivate(event) {
     console.info('Service worker: Activate');
 
