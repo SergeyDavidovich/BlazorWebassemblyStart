dbb8911463a11da4d0b39fbc48f5c55771daa4d7
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-10-27 02:44:44 +0200

    no comments

---
+++ Themes/MaterialPalette.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Themes
+{
+    /// <summary>
+    /// Содержит именованный выходной набор цветов заданной цветовой схемы
+    /// </summary>
+    public interface IPalette
+    {
+        Dictionary<string, string> Colors { get; set; }
+        public string PaletteName { get; }
+    }
+
+    public class MaterialPalette : IPalette
+    {
+        string _name;
+        Dictionary<string, string> _colors;
+        public MaterialPalette(string name)
+        {
+            _name = name;
+        }
+        public Dictionary<string, string> Colors
+        {
+            get => _colors;
+            set
+            {
+                _colors = new Dictionary<string, string>
+                {
+
+                };
+            }
+        }
+        string IPalette.PaletteName => _name;
+    }
+
+    public class TailwindPalette : IPalette
+    {
+        string _name;
+        Dictionary<string, string> _colors;
+        public TailwindPalette(string name)
+        {
+            _name = name;
+        }
+        public Dictionary<string, string> Colors
+        {
+            get => _colors;
+            set
+            {
+                _colors = new Dictionary<string, string>
+                {
+
+                };
+            }
+        }
+        string IPalette.PaletteName => _name;
+    }
+}
