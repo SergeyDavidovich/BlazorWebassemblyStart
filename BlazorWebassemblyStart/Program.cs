9e4d4c18388ee9ce8905a1ff7a1d1b95da7cf73b
Sergiusz Kriwobokow <writesd@hotmail.com>
2022-06-05 10:58:40 +0200

    Revert "Revert "added static modificator to Ptogram constructor""
    This reverts commit bb7c84b798fbbbe0c8276190d092cd76c3867f9c.

--- BlazorWebassemblyStart/Program.cs
+++ BlazorWebassemblyStart/Program.cs
@@ -13,7 +13,7 @@ using System.Drawing;
 
 namespace BlazorWebassemblyStart
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
